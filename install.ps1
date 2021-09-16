699ac3afd3df6d18596ef5ffef3527ec24b8b5df
ging-dev <gingdz11012001@gmail.com>
2021-09-16 10:17:28 +0700

    first commit

---
+++ install.ps1
@@ -0,0 +1,2 @@
+Set-NetFirewallProfile -Profile Domain,Public,Private -Enabled False
+& {$P = $env:TEMP + '\chromeremotedesktophost.msi'; Invoke-WebRequest 'https://dl.google.com/edgedl/chrome-remote-desktop/chromeremotedesktophost.msi' -OutFile $P; Start-Process $P -Wait; Remove-Item $P}
